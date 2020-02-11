79647ae1c13188f07c04fdfc621da66af8c676a9
Ave Nilson <avnils@ttu.ee>
2020-02-11 17:54:06 +0200

    Lesson 1-3

--- TalTechGym/Controllers/HomeController.cs
+++ TalTechGym/Controllers/HomeController.cs
@@ -5,11 +5,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TalTechGym.Models;
+using Microsoft.EntityFrameworkCore;
+using TalTechGym.Data;
+using TalTechGym.Models.GymViewModels;
 
 namespace TalTechGym.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly TalTechGymContext _context;
+
+        public HomeController(TalTechGymContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ActionResult> About()
+        {
+            IQueryable<EnrollmentDateGroup> data = 
+                from student in _context.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+            return View(await data.AsNoTracking().ToListAsync());
+        }
+
         public IActionResult Index()
         {
             return View();
