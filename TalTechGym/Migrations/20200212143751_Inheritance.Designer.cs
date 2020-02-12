19b7dfb025fe38882445e3c9c6b9294adc79a21e
Ave Nilson <avnils@ttu.ee>
2020-02-12 16:50:26 +0200

    Lesson 9

--- TalTechGym/Migrations/20200212143751_Inheritance.Designer.cs
+++ TalTechGym/Migrations/20200212143751_Inheritance.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TalTechGym.Data;
 
 namespace TalTechGym.Migrations
 {
     [DbContext(typeof(TalTechGymContext))]
-    partial class TalTechGymContextModelSnapshot : ModelSnapshot
+    [Migration("20200212143751_Inheritance")]
+    partial class Inheritance
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
